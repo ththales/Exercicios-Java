a0ab24717b35af070ba4847f8493943ca75263e0
Th <ththalesalves05b@gmail.com>
2025-09-25 21:15:24 -0300

    Update ex05.java

--- 01_Logica_de_Programacao/ex05.java
+++ 01_Logica_de_Programacao/ex05.java
@@ -1,4 +1,4 @@
-// Declare uma variável ativo e altere seu valor de true para false.
+// 05) Declare uma variável ativo e altere seu valor de true para false.
 
 public class Solution {
 	public static void main(String[] args) {
