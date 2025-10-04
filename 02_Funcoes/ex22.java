a647f929a40ce82bf9d983c67c910b0339107b4e
Th <ththalesalves05b@gmail.com>
2025-10-03 23:30:58 -0300

    Create ex22.java

---
+++ 02_Funcoes/ex22.java
@@ -0,0 +1,24 @@
+package ex22;
+
+public class Solution {
+	public static int fibonacciRecursivo(int n) {
+		if(n==0) {
+			return 0;
+		}
+		if(n==1) {
+			return 1;
+		}
+		return fibonacciRecursivo(n-1)+fibonacciRecursivo(n-2);
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int index = scan.nextInt();
+		
+		scan.close();
+		
+		int fib = fibonacciRecursivo(index);
+		
+		System.out.println(fib);
+	}
+}
